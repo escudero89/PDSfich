21bd40ac3191276a1d79c1a4e6d739e2553aac31
Cristian Escudero <escudero89@gmail.com>
2012-04-18 22:03:15 -0300

    Agregada unas modificaciones y un par de tips de fern17

--- Three/Cristian/eje1.m
+++ Three/Cristian/eje1.m
@@ -1,20 +1,19 @@
 #! /usr/bin/octave -qf
 1;
 
-# Funcion basica que te devuelve la transformada y acomodada
-function [fou, fou_acom] = get_fourier(senial)
+# Funcion basica que te devuelve la transformada acomodada
+function [fou] = get_fourier(senial)
     
     N = length(senial);
-    
-    fou = fft(senial);
-    fou_acom = [fou(1, N/2+1 : N), fou(1, 1 : N/2)];
+        
+    fou = fftshift(fft(senial)); # la transforma y luego la acomoda
     
 endfunction
 
 # Retorno un vector de phi para hacer el espectro de fase
 function [fase] = get_fase(senial_transf)
 
-    fase = atan( imag(senial_transf) ./ real(senial_transf));
+    fase = atan(-1 .* imag(senial_transf) ./ real(senial_transf));
 
 endfunction
 
@@ -37,7 +36,7 @@ s_t = sin(2 * pi * f1 * t) + 4 * sin(2 * pi * f2 * t);
 
 # Calculamos la TDF s_k y la mostramos
 
-[s_k, s_k_acomodado] = get_fourier(s_t);
+s_k = get_fourier(s_t);
 
 # Vemos si se cumple lo de Parseval
 
@@ -53,12 +52,14 @@ endif
 
 s_t_uno = s_t .+ 4;
 
-[s_k_uno, s_k_uno_acom] = get_fourier(s_t_uno);
+s_k_uno = get_fourier(s_t_uno);
 
 
 # Graficamos todo. Si queremos fase o magnitud modificar booleano
 
-magnitud = false;
+magnitud = 0;
+
+clg; # borramos todo lo que hay ploteado y a la bosta
 
 hold on;
 
@@ -66,17 +67,17 @@ if magnitud == true
 
     disp "Espectros de Magnitud"
 
-    plot(t, abs(s_k_acomodado), 'r');
-
-    plot(t, abs(s_k_uno_acom), 'g');
+    stem(t, abs(s_k_uno), 'g');
+    
+    stem(t, abs(s_k), 'r');    
 
 else
 
     disp "Espectros de Fases"
 
-    plot(t, get_fase(s_k_acomodado), 'r');
+    stem(t, get_fase(s_k), 'r');
 
-    plot(t, get_fase(s_k_uno_acom), 'g');        
+    stem(t, get_fase(s_k_uno), 'g');        
     
 endif
 
