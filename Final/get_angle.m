20924fa999ddcada0c651bf6fe80ddb6b84eddb2
Cristian Escudero <escudero89@gmail.com>
2012-06-02 17:47:57 -0300

    True en vez de false.

--- Final/get_angle.m
+++ Final/get_angle.m
@@ -3,7 +3,7 @@
 # Le pasas el delta espacio, y el d de separacion entre microfonos.
 # Te retorna el angulo al que esta la fuente de sonido.
 
-function [alpha] = get_angle( dE, d, degree = false )
+function [alpha] = get_angle( dE, d, degree = true )
 
     Beta = asin(dE / d);
 
