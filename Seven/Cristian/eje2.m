d3a0b0c8298c65caeed8a83be8ee36fc53636f9a
Cristian Escudero <escudero89@gmail.com>
2012-06-05 10:29:10 -0300

    Ejercicio terminado.

--- Seven/Cristian/eje2.m
+++ Seven/Cristian/eje2.m
@@ -4,7 +4,6 @@ clear all;
 
 function [s_k] = get_polos(N)
     
-    d = 1e-6;
     techo = ceil(N/2);
 
     # for con N/2 pares, (N+1)/2 impares
@@ -12,7 +11,7 @@ function [s_k] = get_polos(N)
         alpha = (2*k - 1)/(2*N)*pi;
         s_k(k) = -sin(alpha) + j * cos(alpha);
         
-        if (imag(s_k(k)) > d)
+        if (imag(s_k(k)) > eps)
             s_k(k + techo) = -sin(alpha) - j * cos(alpha);
         endif
         
@@ -24,7 +23,7 @@ endfunction
 
 # Definimos variables iniciales
 
-w_c = 500; # frecuencia de corte
+wc = 500; # frecuencia de corte
 
 fm = 2000;
 Tm = 1 / fm;
@@ -33,19 +32,23 @@ t = 0 : Tm : 1 - Tm;
 
 N = 3; # orden
 
-##############3
+##############
 
 H_z = [];
 polos = get_polos(N);
 
-circulo = e.^(j*2*pi*t);
+w_c = 2 / Tm * tan(pi * wc * Tm);
+
+circulo = e.^(j * 2 * pi * t);
 
 for i = 1 : length(circulo)
     
-    z = circulo(i);    
+    z = circulo(i);
    
-    s = 2/Tm * ((1 - z^-1)/(1 + z^-1));
+    s = 2/Tm * ((1 - z^-1)/(1 + z^-1));   
    
-    H_z(i) = 1 / prod(s - polos);    
+    ganancia = (wc * s)^length(polos) / prod(-polos .* wc);
+   
+    H_z(i) = ganancia / prod(s - w_c./(polos));    
         
 endfor
