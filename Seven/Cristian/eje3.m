297b37c3bca0b1c23cafa4d5ee039f4f66746dda
Cristian Escudero <escudero89@gmail.com>
2012-06-11 11:36:17 -0300

    Mira de nuevo.

--- Seven/Cristian/eje3.m
+++ Seven/Cristian/eje3.m
@@ -16,10 +16,27 @@ Rp = 0.7; #db
 Rs = 55; #db
 
 # Calculamos el orden y el cutoff 
-[n, Wc] = buttord([Wp1, Wp2], [Ws1, Ws2], Rp, Rs);
+[n_butt, Wc_butt] = buttord([Wp1, Wp2], [Ws1, Ws2], Rp, Rs);
+
+[n_cheb1, Wc_cheb1] = cheb1ord([Wp1, Wp2], [Ws1, Ws2], Rp, Rs);
+[n_cheb2, Wc_cheb2] = cheb2ord([Wp1, Wp2], [Ws1, Ws2], Rp, Rs);
+
+[n_ellip, Wc_ellip] = ellipord([Wp1, Wp2], [Ws1, Ws2], Rp, Rs);
+
+# Divido el ordne por dos por wtf
+n_butt = ceil(n_butt/2);
 
 # Generamos los coeficientes del filtro
-[b,a] = butter(n, Wc);
+[b_butt, a_butt] = butter(n_butt, Wc_butt);
 
-freqz(b,a,fm/2);
+[b_cheb1, a_cheb1] = cheb(n_cheb1, Wc_cheb1);
+[b_cheb2, a_cheb2] = cheb(n_cheb2, Wc_cheb2);
 
+[b_ellip, a_ellip] = ellip(n_ellip, Wc_ellip);
+
+clf;
+hold on;
+plot(abs(freqz(b_butt, a_butt, fm/2)));
+
+
+hold off;
